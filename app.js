b960b8df5c4c0de3ca75c91de249da8bff95a601
Ameka Brown <brownameka05@gmail.com>
2018-11-12 09:59:58 -0600

    creating rep for Houston water level project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mustacheExpress = require('mustache-express')
+
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static('public'))
+app.engine('mustache', mustacheExpress())
+app.set("views", "./views")
+app.set("view engine", "mustache")
