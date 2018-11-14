22189ac58d4521baf62158a15c6445c4081200c9
Emre Akurek <emreyeprem@gmail.com>
2018-11-14 13:34:55 -0600

    another update

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+// const express = require('express')
+// const app = express()
+// const bodyParser = require('body-parser')
+// const mustacheExpress = require('mustache-express')
+//
+//
+// app.use(bodyParser.urlencoded({extended:false}))
+// app.use(express.static('public'))
+// app.engine('mustache', mustacheExpress())
+// app.set("views", "./views")
+// app.set("view engine", "mustache")
+// ========================================
 
 const updateDaily = require('./dailyimports')
 const express = require('express')
@@ -30,55 +42,9 @@ app.get('/',function(req,res){
 
 let lakeHouston = []
 let lakeBuffalo = []
-let recentDataOfHoustonLake = []  // DATA OF LAST 10 DAYS
-let recentDataOfBuffaloLake = []
-// https://waterservices.usgs.gov/nwis/iv/?site=08072300,08072000&format=json&parameterCd=00065&period=PT240H
 
 const https = require("https");
-
-const url = "https://waterservices.usgs.gov/nwis/iv/?site=08072000&format=json&parameterCd=00065&startDT=2018-11-13&endDT=2018-11-14";
-
-https.get(url, res => {
-  res.setEncoding("utf8");
-  let body = "";
-  res.on("data", data => {
-    body += data;
-  });
-  var count = 71
-  res.on("end", () => {
-    body = JSON.parse(body);
-    let dataObject = body.value.timeSeries[0].values[0].value
-    let siteName = "Houston Lake"
-    let siteId = 08072000
-    // lakeHouston = []
-    dataObject.forEach(function(each){
-      let height = each.value
-      let dateTime = each.dateTime
-      let dataOfHoustonLake = {height:height, dateTime:dateTime}
-      // console.log(dataOfHoustonLake)
-      lakeHouston.push(dataOfHoustonLake)
-
-    })
-        // console.log(lakeHouston)
-      let dataOfDay1 = lakeHouston.slice(49,50)
-      // console.log(dataOfDay1)
-      let dataOfDay2 = lakeHouston.slice(109,110)
-
-      recentDataOfHoustonLake.push(dataOfDay2)
-      // console.log(recentDataOfHoustonLake)
-      recentDataOfHoustonLake.push(dataOfDay1)
-
-      // console.log(recentDataOfHoustonLake)
-
-      // let recentDataOfHouston = lakeHouston.slice((lakeHouston.length-10), lakeHouston.length)
-      // console.log(recentDataOfHouston)
-
-      recentDataOfHoustonLake.forEach(function(each){
-        count++
-        let waterHeight = each[0].height
-        console.log(waterHeight)
-=======
-const url = "https://waterservices.usgs.gov/nwis/iv/?site=08072300,08072000&format=json&parameterCd=00065&period=PT26H";
+const url = "https://waterservices.usgs.gov/nwis/iv/?site=08072300,08072000&format=json&parameterCd=00065&period=PT30H";
 
 app.get('/recentData', (req,response)=>{
   https.get(url, res => {
@@ -90,22 +56,27 @@ app.get('/recentData', (req,response)=>{
     var count = 31
     res.on("end", () => {
       body = JSON.parse(body);
-      let dataObject = body.value.timeSeries[0].values[0].value
-      let siteName = "Houston Lake"
-      let siteId = 0807200
-      lakeHouston = []
-      dataObject.forEach(function(each){
-        let height = each.value
-        let dateTime = each.dateTime
-        let dataOfHoustonLake = {height:height, dateTime:dateTime}
+      const dataObjectHouston = body.value.timeSeries[0].values[0].value
+      const dataObjectBuffalo = body.value.timeSeries[1].values[0].value
+      let lakeHouston = []
+      let buffaloBayou = []
+      dataObjectHouston.forEach(function(each){
+        const height = each.value
+        const dateTime = each.dateTime
+        const dataOfHoustonLake = {height:height, dateTime:dateTime}
         lakeHouston.push(dataOfHoustonLake)
-
+      })
+      dataObjectBuffalo.forEach(function(each){
+        const height = each.value
+        const dateTime = each.dateTime
+        const dataOfBuffaloBayou = {height:height, dateTime:dateTime}
+        buffaloBayou.push(dataOfBuffaloBayou)
       })
 
-        let recentDataOfLakeHouston = lakeHouston.slice((lakeHouston.length-96), lakeHouston.length)
-        // console.log(recentDataOfLakeHouston)
+        const recentDataOfLakeHouston = lakeHouston.slice(lakeHouston.length >= 96 ? (lakeHouston.length-96) : 0, lakeHouston.length)
+        const recentDataOfBuffaloBayou = buffaloBayou.slice(buffaloBayou.length >= 96 ? (buffaloBayou.length-96) : 0, buffaloBayou.length)
 
-        response.send(JSON.stringify(recentDataOfLakeHouston))
+        response.send(JSON.stringify({houston:recentDataOfLakeHouston,buffalo:recentDataOfBuffaloBayou}))
 
         // recentDataOfHouston.forEach(function(each){
         //   count++
@@ -137,17 +108,3 @@ app.get('/recentData', (req,response)=>{
 
 
 updateDaily(db);
-
-
-        let recordedDate = each[0].dateTime
-        console.log(recordedDate)
-
-
-
-
-  })
-
-    // =========================================================
-
-  })   // end of line 58
-// })     // https === end of line 51
