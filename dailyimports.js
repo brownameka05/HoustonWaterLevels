b1e04ec0b823951f5907c227c39342bb4f7741ce
Jack Gamboa <jgamboa929@gmail.com>
2018-11-15 10:57:46 -0600

    updated dailyimports with new daily code

--- dailyimports.js
+++ dailyimports.js
@@ -1,4 +1,4 @@
-
+const fetch = require('node-fetch');
 function updateDaily(db){
 
     // 1 Get missing last datetimes per lake per day from db
@@ -6,7 +6,7 @@ function updateDaily(db){
                     , max(DATE(w.date)) + 1 as lastmaxdateentry
                     , DATE(current_timestamp) as currentdatetime
                 from waterheights w
-                group by w.siteid, current_timestamp
+                group by w.siteid, DATE(current_timestamp)
                 having max(DATE(w.date)) + 1 < DATE(current_timestamp)`
 
     db.any(sql)
@@ -30,6 +30,7 @@ function updateDaily(db){
                     siteUrls.push(url)
                 });
 
+                    console.log(siteUrls)
                 // 3 Get one gage value per day per site
                 oneDayData(siteUrls, db)
             }
@@ -46,6 +47,49 @@ function oneDayData(siteUrls, db) {
     // Loop through built url api calls for missing dates
     for (let i=0; i < siteUrls.length; i ++) {
 
+        const url = siteUrls[i]
+
+        // const connectionString = process.env.DB_CONN
+        // const db = pgp(config)
+
+        fetch(url)
+            .then(result => {
+                return result.json();
+            })
+            .then((data)=> {
+                const valueArray = data.value.timeSeries[0].values[0].value;
+                let siteName = data.value.timeSeries[0].sourceInfo.siteName
+                let siteId = data.value.timeSeries[0].sourceInfo.siteCode[0].value
+
+                // let waterHeight = each[0].height
+                // let recordedDate = each[0].dateTime.replace('T',' ')
+
+                let previousDate = ''
+                let promises = []
+                valueArray.forEach((valueObject,index) => {
+                    const height = valueObject.value
+                    const dateTimeStr = valueObject.dateTime
+                    const dateTimeObj = new Date(Date.parse(dateTimeStr))
+                    
+                    const dateCompare = dateTimeObj.getFullYear() + dateTimeObj.getMonth() + dateTimeObj.getDate()
+                    
+                    if(dateCompare != previousDate)
+                    {
+                        promises.push(db.any('INSERT INTO waterheights (siteid,sitename,height,date) VALUES ($1,$2,$3,$4)',[siteId,siteName,height,dateTimeStr.replace('T',' ')]))
+                        // promises.push(db.any('INSERT INTO waterheights (siteid,sitename,height,date) VALUES(08072000, \'Lake Houston\', ' + height + ', \'' + dateTimeStr.replace('T',' ') + '\')'))
+                    }
+
+                    previousDate = dateCompare
+                })
+                Promise.all(promises)
+                .then(after => console.log("DONE"))
+            })
+            .catch(err => console.log(err));
+
+
+
+
+        /*
         let sitesArray = []
         let recentDataOfSites = [] 
 
@@ -97,6 +141,10 @@ function oneDayData(siteUrls, db) {
                 })
             }) 
         })
+
+        */
+
+
     }
 }
 
